e45e72b31b92a77d16c8f76c572984787f338884
John Ellefson <johnellefson@byui.edu>
2024-09-20 11:54:44 -0500

    Fix addProductToCart function, update lint rules

--- src/js/product.js
+++ src/js/product.js
@@ -4,21 +4,19 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  
-  //get cart items from local storage
-  let existingCart = getLocalStorage('so-cart');
-  
-  //If the cart is not an array, cause it has no items initialize it as an array empty
-  
-  if (!Array.isArray(existingCart)){
+  // get cart items from local storage
+  let existingCart = getLocalStorage("so-cart");
+
+  // if the cart is not an array because it has no items, initialize it as an array empty
+  if (!Array.isArray(existingCart)) {
     existingCart = [];
   }
- 
-  // Add the products
-  
-  existingCart.push(product)
-  setLocalStorage('so-cart', existingCart)
+
+  // add the products to local storage
+  existingCart.push(product);
+  setLocalStorage("so-cart", existingCart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
