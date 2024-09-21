b270645c82eec32c9d2604a5252313af23fd6835
jrgerodriguez <menjivar.jorge.93@gmail.com>
2024-09-20 21:22:42 -0600

    individual1 activity done

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartArray = getLocalStorage("so-cart") || [];
+  cartArray.push(product);
+  setLocalStorage("so-cart", cartArray);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
