909dceea34f07b6681566f5203242fb807a381e6
John Ellefson <johnellefson@byui.edu>
2024-09-25 22:29:43 -0500

    Work in progress

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,34 @@
+function productCardTemplate(product) {
+    return `<li class="product-card">
+      <a href="product_pages/index.html?product=">
+        <img src="" alt="Image of ">
+        <h3 class="card__brand"></h3>
+        <h2 class="card__name"></h2>
+        <p class="product-card__price">$</p>
+      </a>
+    </li>`
+}
+
+export default class ProductListing {
+    constructor(category, dataSource, listElement) {
+      // We passed in this information to make our class as reusable as possible.
+      // Being able to define these things when we use the class will make it very flexible
+      this.category = category;
+      this.dataSource = dataSource;
+      this.listElement = listElement;
+    }
+
+    async init() {
+      // our dataSource will return a Promise...so we can use await to resolve it.
+      const list = await this.dataSource.getData();
+      // render the list - to be completed
+    }
+
+    renderList() {
+      // Loop through the list and create a card for each item
+      // Append each card to the listElement
+      
+    }
+}
+
+  
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import { ProductData } from "./ProductData.mjs";
+import { ProductListing } from "./ProductList.mjs";
+
+// Create an instance of ProductData object
+const productData = new ProductData("tents");
+const productListing = new ProductListing();
